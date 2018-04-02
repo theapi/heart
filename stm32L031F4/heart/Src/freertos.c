96e3cee43516cdc2d9fe4dfff547b57aa1e87472
theapi <petermclarke@gmail.com>
2018-04-02 17:36:07 +0100

    FreeRTOS

---
+++ stm32L031F4/heart/Src/freertos.c
@@ -0,0 +1,220 @@
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2018 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "cmsis_os.h"
+
+/* USER CODE BEGIN Includes */     
+#include "gpio.h"
+#include "tim.h"
+#include "main.h"
+
+/* USER CODE END Includes */
+
+/* Variables -----------------------------------------------------------------*/
+osThreadId buttonHandle;
+osThreadId led1Handle;
+
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+
+/* Function prototypes -------------------------------------------------------*/
+void buttonTask(void const * argument);
+void led1Task(void const * argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+/* Hook prototypes */
+
+/* Init FreeRTOS */
+
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+       
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the thread(s) */
+  /* definition and creation of button */
+  osThreadDef(button, buttonTask, osPriorityNormal, 0, 128);
+  buttonHandle = osThreadCreate(osThread(button), NULL);
+
+  /* definition and creation of led1 */
+  osThreadDef(led1, led1Task, osPriorityNormal, 0, 128);
+  led1Handle = osThreadCreate(osThread(led1), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+}
+
+/* buttonTask function */
+void buttonTask(void const * argument)
+{
+
+  /* USER CODE BEGIN buttonTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    /* For now just toggel a led */
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+
+    osDelay(500);
+  }
+  /* USER CODE END buttonTask */
+}
+
+/* led1Task function */
+void led1Task(void const * argument)
+{
+  /* USER CODE BEGIN led1Task */
+
+  /* Index of the sine table */
+  uint8_t sine_index = 0;
+  /* Sine wave table */
+  const uint8_t sine_wave[256] = {
+    0x80, 0x83, 0x86, 0x89, 0x8C, 0x90, 0x93, 0x96,
+    0x99, 0x9C, 0x9F, 0xA2, 0xA5, 0xA8, 0xAB, 0xAE,
+    0xB1, 0xB3, 0xB6, 0xB9, 0xBC, 0xBF, 0xC1, 0xC4,
+    0xC7, 0xC9, 0xCC, 0xCE, 0xD1, 0xD3, 0xD5, 0xD8,
+    0xDA, 0xDC, 0xDE, 0xE0, 0xE2, 0xE4, 0xE6, 0xE8,
+    0xEA, 0xEB, 0xED, 0xEF, 0xF0, 0xF1, 0xF3, 0xF4,
+    0xF5, 0xF6, 0xF8, 0xF9, 0xFA, 0xFA, 0xFB, 0xFC,
+    0xFD, 0xFD, 0xFE, 0xFE, 0xFE, 0xFF, 0xFF, 0xFF,
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFE, 0xFE, 0xFD,
+    0xFD, 0xFC, 0xFB, 0xFA, 0xFA, 0xF9, 0xF8, 0xF6,
+    0xF5, 0xF4, 0xF3, 0xF1, 0xF0, 0xEF, 0xED, 0xEB,
+    0xEA, 0xE8, 0xE6, 0xE4, 0xE2, 0xE0, 0xDE, 0xDC,
+    0xDA, 0xD8, 0xD5, 0xD3, 0xD1, 0xCE, 0xCC, 0xC9,
+    0xC7, 0xC4, 0xC1, 0xBF, 0xBC, 0xB9, 0xB6, 0xB3,
+    0xB1, 0xAE, 0xAB, 0xA8, 0xA5, 0xA2, 0x9F, 0x9C,
+    0x99, 0x96, 0x93, 0x90, 0x8C, 0x89, 0x86, 0x83,
+    0x80, 0x7D, 0x7A, 0x77, 0x74, 0x70, 0x6D, 0x6A,
+    0x67, 0x64, 0x61, 0x5E, 0x5B, 0x58, 0x55, 0x52,
+    0x4F, 0x4D, 0x4A, 0x47, 0x44, 0x41, 0x3F, 0x3C,
+    0x39, 0x37, 0x34, 0x32, 0x2F, 0x2D, 0x2B, 0x28,
+    0x26, 0x24, 0x22, 0x20, 0x1E, 0x1C, 0x1A, 0x18,
+    0x16, 0x15, 0x13, 0x11, 0x10, 0x0F, 0x0D, 0x0C,
+    0x0B, 0x0A, 0x08, 0x07, 0x06, 0x06, 0x05, 0x04,
+    0x03, 0x03, 0x02, 0x02, 0x02, 0x01, 0x01, 0x01,
+    0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x03,
+    0x03, 0x04, 0x05, 0x06, 0x06, 0x07, 0x08, 0x0A,
+    0x0B, 0x0C, 0x0D, 0x0F, 0x10, 0x11, 0x13, 0x15,
+    0x16, 0x18, 0x1A, 0x1C, 0x1E, 0x20, 0x22, 0x24,
+    0x26, 0x28, 0x2B, 0x2D, 0x2F, 0x32, 0x34, 0x37,
+    0x39, 0x3C, 0x3F, 0x41, 0x44, 0x47, 0x4A, 0x4D,
+    0x4F, 0x52, 0x55, 0x58, 0x5B, 0x5E, 0x61, 0x64,
+    0x67, 0x6A, 0x6D, 0x70, 0x74, 0x77, 0x7A, 0x7D
+  };
+
+  /* Initialise all the PWM timers */
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim21, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim21, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim22, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim22, TIM_CHANNEL_2);
+
+  /*
+   * Fade all the leds
+   */
+  for(;;)
+  {
+
+    // PA1     ------> TIM2_CH2
+    htim2.Instance->CCR2 = sine_wave[sine_index];
+    // PA2     ------> TIM21_CH1
+    htim21.Instance->CCR1 = sine_wave[sine_index];
+    // PA3     ------> TIM21_CH2
+    htim21.Instance->CCR2 = sine_wave[sine_index];
+    // PA5     ------> TIM2_CH1
+    htim2.Instance->CCR1 = sine_wave[sine_index];
+    // PA6     ------> TIM22_CH1
+    htim22.Instance->CCR1 = sine_wave[sine_index];
+    // PA7     ------> TIM22_CH2
+    htim22.Instance->CCR2 = sine_wave[sine_index];
+
+    // Increment and let overflow back to 0.
+    sine_index++;
+
+    // Come back to this loop later.
+    osDelay(10);
+  }
+
+  /* USER CODE END led1Task */
+}
+
+/* USER CODE BEGIN Application */
+     
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- stm32L031F4/heart/Src/main.c
+++ stm32L031F4/heart/Src/main.c
@@ -4,41 +4,52 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  ** This notice applies to any and all portions of this file
+  * This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
   * USER CODE END. Other portions of this file, whether 
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
-  * COPYRIGHT(c) 2018 STMicroelectronics
+  * Copyright (c) 2018 STMicroelectronics International N.V. 
+  * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
   *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
   */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32l0xx_hal.h"
+#include "cmsis_os.h"
 #include "tim.h"
 #include "gpio.h"
 
@@ -55,6 +66,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+void MX_FREERTOS_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -99,56 +111,15 @@ int main(void)
   MX_TIM22_Init();
   /* USER CODE BEGIN 2 */
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
-
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-  HAL_TIM_PWM_Start(&htim21, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim21, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim22, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim22, TIM_CHANNEL_2);
-
-  uint32_t fade1 = 0;
-  uint8_t sine_index = 0;
-  uint8_t direction = 0;
-
-  uint8_t  sine_wave[256] = {
-  0x80, 0x83, 0x86, 0x89, 0x8C, 0x90, 0x93, 0x96,
-  0x99, 0x9C, 0x9F, 0xA2, 0xA5, 0xA8, 0xAB, 0xAE,
-  0xB1, 0xB3, 0xB6, 0xB9, 0xBC, 0xBF, 0xC1, 0xC4,
-  0xC7, 0xC9, 0xCC, 0xCE, 0xD1, 0xD3, 0xD5, 0xD8,
-  0xDA, 0xDC, 0xDE, 0xE0, 0xE2, 0xE4, 0xE6, 0xE8,
-  0xEA, 0xEB, 0xED, 0xEF, 0xF0, 0xF1, 0xF3, 0xF4,
-  0xF5, 0xF6, 0xF8, 0xF9, 0xFA, 0xFA, 0xFB, 0xFC,
-  0xFD, 0xFD, 0xFE, 0xFE, 0xFE, 0xFF, 0xFF, 0xFF,
-  0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0xFE, 0xFE, 0xFD,
-  0xFD, 0xFC, 0xFB, 0xFA, 0xFA, 0xF9, 0xF8, 0xF6,
-  0xF5, 0xF4, 0xF3, 0xF1, 0xF0, 0xEF, 0xED, 0xEB,
-  0xEA, 0xE8, 0xE6, 0xE4, 0xE2, 0xE0, 0xDE, 0xDC,
-  0xDA, 0xD8, 0xD5, 0xD3, 0xD1, 0xCE, 0xCC, 0xC9,
-  0xC7, 0xC4, 0xC1, 0xBF, 0xBC, 0xB9, 0xB6, 0xB3,
-  0xB1, 0xAE, 0xAB, 0xA8, 0xA5, 0xA2, 0x9F, 0x9C,
-  0x99, 0x96, 0x93, 0x90, 0x8C, 0x89, 0x86, 0x83,
-  0x80, 0x7D, 0x7A, 0x77, 0x74, 0x70, 0x6D, 0x6A,
-  0x67, 0x64, 0x61, 0x5E, 0x5B, 0x58, 0x55, 0x52,
-  0x4F, 0x4D, 0x4A, 0x47, 0x44, 0x41, 0x3F, 0x3C,
-  0x39, 0x37, 0x34, 0x32, 0x2F, 0x2D, 0x2B, 0x28,
-  0x26, 0x24, 0x22, 0x20, 0x1E, 0x1C, 0x1A, 0x18,
-  0x16, 0x15, 0x13, 0x11, 0x10, 0x0F, 0x0D, 0x0C,
-  0x0B, 0x0A, 0x08, 0x07, 0x06, 0x06, 0x05, 0x04,
-  0x03, 0x03, 0x02, 0x02, 0x02, 0x01, 0x01, 0x01,
-  0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x03,
-  0x03, 0x04, 0x05, 0x06, 0x06, 0x07, 0x08, 0x0A,
-  0x0B, 0x0C, 0x0D, 0x0F, 0x10, 0x11, 0x13, 0x15,
-  0x16, 0x18, 0x1A, 0x1C, 0x1E, 0x20, 0x22, 0x24,
-  0x26, 0x28, 0x2B, 0x2D, 0x2F, 0x32, 0x34, 0x37,
-  0x39, 0x3C, 0x3F, 0x41, 0x44, 0x47, 0x4A, 0x4D,
-  0x4F, 0x52, 0x55, 0x58, 0x5B, 0x5E, 0x61, 0x64,
-  0x67, 0x6A, 0x6D, 0x70, 0x74, 0x77, 0x7A, 0x7D
-};
-
   /* USER CODE END 2 */
+
+  /* Call init function for freertos objects (in freertos.c) */
+  MX_FREERTOS_Init();
+
+  /* Start scheduler */
+  osKernelStart();
+  
+  /* We should never get here as control is now taken by the scheduler */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -158,56 +129,6 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
-
-    if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4)) {
-      // Change fade if button not pressed. It's pull high when not pressed.
-
-      if (direction == 0) {
-          fade1++;
-      } else {
-          fade1--;
-      }
-
-      sine_index++;
-      if (sine_index > 255) {
-        sine_index = 0;
-      }
-
-      if (fade1 == 255) {
-        direction = 1;
-        //HAL_Delay(1000);
-      } else if (fade1 == 50) {
-        //HAL_Delay(500);
-        direction = 0;
-      }
-
-      /*
-       * Fade all the leds
-       */
-
-      // PA1     ------> TIM2_CH2
-      //htim2.Instance->CCR2 = fade1;
-
-      htim2.Instance->CCR2 = sine_wave[sine_index];
-
-      // PA2     ------> TIM21_CH1
-      //htim21.Instance->CCR1 = fade1;
-      htim21.Instance->CCR1 = sine_wave[sine_index];
-      // Opposite direction.
-      //htim21.Instance->CCR1 = 255 - fade1;
-      // PA3     ------> TIM21_CH2
-      //htim21.Instance->CCR2 = fade1;
-      htim21.Instance->CCR2 = sine_wave[sine_index];
-
-      // PA5     ------> TIM2_CH1
-      htim2.Instance->CCR1 = fade1;
-      // PA6     ------> TIM22_CH1
-      htim22.Instance->CCR1 = fade1;
-      // PA7     ------> TIM22_CH2
-      htim22.Instance->CCR2 = fade1;
-    }
-
-    HAL_Delay(30);
 
   }
   /* USER CODE END 3 */
@@ -263,7 +184,7 @@ void SystemClock_Config(void)
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
   /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(SysTick_IRQn, 3, 0);
 }
 
 /* USER CODE BEGIN 4 */
